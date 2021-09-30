97dfcb6da21ec9eaa611ed388b0f40fb5f7278d4
Jeremy <peace@trippyogi.com>
2021-09-29 18:13:27 -0700

    Add Nibiru character

--- src/game.ts
+++ src/game.ts
@@ -1,33 +1,33 @@
-import * as utils from '@dcl/ecs-scene-utils'
-import { Arissa } from './arissa'
+import * as utils from '@dcl/ecs-scene-utils';
+import { NibiruCharacter } from './nibiru-character';
 
 // Base
-const base = new Entity()
-base.addComponent(new GLTFShape('models/baseGrass.glb'))
-engine.addEntity(base)
+const base = new Entity();
+base.addComponent(new GLTFShape('models/baseGrass.glb'));
+engine.addEntity(base);
 
 // Arissa
-const arissa = new Arissa(
-  new GLTFShape('models/arissa.glb'),
+const nibiruCharacter = new NibiruCharacter(
+  new GLTFShape('models/Hooded.glb'),
   new Transform({
-    position: new Vector3(0, 0.05, -0.1),
-    scale: new Vector3(0, 0, 0),
+    position: new Vector3(0, -0.9, -0.3),
+    scale: new Vector3(0, 0, 0)
   })
-)
-arissa.setParent(Attachable.AVATAR)
+);
+nibiruCharacter.setParent(Attachable.AVATAR);
 
 // Hide avatars
-const hideAvatarsEntity = new Entity()
+const hideAvatarsEntity = new Entity();
 hideAvatarsEntity.addComponent(
   new AvatarModifierArea({
     area: { box: new Vector3(16, 4, 11) },
-    modifiers: [AvatarModifiers.HIDE_AVATARS],
+    modifiers: [AvatarModifiers.HIDE_AVATARS]
   })
-)
+);
 hideAvatarsEntity.addComponent(
   new Transform({ position: new Vector3(8, 2, 10.5) })
-)
-engine.addEntity(hideAvatarsEntity)
+);
+engine.addEntity(hideAvatarsEntity);
 
 // Create to show Arissa avatar
 hideAvatarsEntity.addComponent(
@@ -35,28 +35,26 @@ hideAvatarsEntity.addComponent(
     new utils.TriggerBoxShape(new Vector3(16, 4, 11), Vector3.Zero()),
     {
       onCameraEnter: () => {
-        arissa.getComponent(Transform).scale.setAll(1)
+        nibiruCharacter.getComponent(Transform).scale.setAll(1.5);
       },
       onCameraExit: () => {
-        arissa.getComponent(Transform).scale.setAll(0)
-      },
+        nibiruCharacter.getComponent(Transform).scale.setAll(0);
+      }
     }
   )
-)
+);
 
 // Check if player is moving
-let currentPosition = new Vector3()
-
-
+let currentPosition = new Vector3();
 
 class CheckPlayerIsMovingSystem implements ISystem {
   update() {
     if (currentPosition.equals(Camera.instance.position)) {
-      arissa.playIdle()
+      nibiruCharacter.playIdle();
     } else {
-      currentPosition.copyFrom(Camera.instance.position)
-      arissa.playRunning()
+      currentPosition.copyFrom(Camera.instance.position);
+      nibiruCharacter.playRunning();
     }
   }
 }
-engine.addSystem(new CheckPlayerIsMovingSystem())
+engine.addSystem(new CheckPlayerIsMovingSystem());
